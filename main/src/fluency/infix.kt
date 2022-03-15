610fcc822116db3de783f55d779ddea391653b8c
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-03-14 21:08:08 -0400

    Added infix operation

---
+++ main/src/fluency/infix.kt
@@ -0,0 +1,6 @@
+package fluency
+ object fetch {
+     infix fun balance(number:Int) = println("Fetch the balance for number ${number}")
+ }
+
+// So you can do something like this fetch balance 12345
