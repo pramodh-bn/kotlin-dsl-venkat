92cb4f859f77adbb0d42b9d96096441666e002e4
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-03-20 13:35:06 -0400

    Added register context to fix ambiguious and

--- main/src/context/register.kts
+++ main/src/context/register.kts
@@ -1,3 +1,12 @@
 object register {
-
+    infix fun add(op1: Int): AndOp {
+        return AndOp(op1)
+    }
+    class AndOp(val total: Int) {
+        infix fun and(op2: Int) {
+            println("Adding $op2 to $total ")
+        }
+    }
 }
+
+register add 4 and 3
