673b10fe77249743488525cc3cfc98ff3361ceaf
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-03-20 13:07:38 -0400

    Added Config and CommandContext classes

--- main/src/context/config.kt
+++ main/src/context/config.kt
@@ -1,9 +1,39 @@
 package context
 // Defining the build function
 fun build(block: Config.() -> Unit) {
-    val configuraton = Config()
-    configuraton.block()
+    val configuration = Config()
+    configuration.block()
     println(configuration)
     // This could be made concise by
     // println(Config().apply(block))
+}
+
+class Config {
+    var version = ""
+    var src = ""
+    var test = ""
+    var tasks = ""
+
+    private val dependencies = mutableListOf<String>()
+
+    fun dependency(library: String) = dependencies.add(library)
+
+    fun task(type:String, commands:CommandContext.() -> Unit) {
+        var commandContext = CommandContext().apply(commands)
+
+        tasks = "$tasks--$type configured with ${commandContext}"
+    }
+
+    override fun toString() = """Config:
+        |Version: $version
+        |Source path: $src
+        |Test path: $test
+        |Dependencies: ${dependencies.joinToString()}
+        |Tasks: $tasks
+    """.trimMargin()
+}
+
+class CommandContext {
+    var jvmArgs = ""
+    override fun toString() = "jvmArgs: $jvmArgs"
 }
