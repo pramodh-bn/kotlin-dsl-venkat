5a4d8fcb68ed85847fa3f25cbf69f2ee8791fec9
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-03-22 09:29:25 -0400

    Added context commentary to the schedule.kts

--- main/src/context/schedule.kts
+++ main/src/context/schedule.kts
@@ -7,3 +7,10 @@ schedule meeting {
     on date 15 March 2022
     participant include "Sara" and "Jake" and "Mani"
 }
+
+// schedule can be a singleton with meeting() as a function that takes lambda as a parameter
+// But what about assign, starts, ends, on, participant. One approach may be to define
+// each one of them as singletons. However that approach as some problems
+// in addition to name, at, date and include we also need to gather the data
+// needed to define a meeting. Arbitrary singletons are not great to carry
+// single representation of data; we need to look further.
