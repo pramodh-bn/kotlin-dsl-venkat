dd59b49a98ac4aca05a4bcf69fcfd349c5cb6830
Pramodh Nanjayya <pramodh.nanjayya@gmail.com>
2022-03-23 09:23:20 -0400

    Moved DateCreator inside Meeting class

--- main/src/context/meeting.kt
+++ main/src/context/meeting.kt
@@ -59,24 +59,23 @@ class Meeting {
             attending = "$attending, $name"
         }
     }
+    class DateCreator(val day: Int, val meeting: Meeting) {
+        private fun setScheduledOn(month: Int, year: Int) {
+            meeting.scheduledOn = java.time.LocalDate.of(year, month, day).toString()
+        }
 
-}
-
-class DateCreator(val day: Int, val meeting: Meeting) {
-    private fun setScheduledOn(month: Int, year: Int) {
-        meeting.scheduledOn = java.time.LocalDate.of(year, month, day).toString()
+        infix fun January(year: Int) = setScheduledOn(1, year)
+        infix fun February(year: Int) = setScheduledOn(2, year)
+        infix fun March(year: Int) = setScheduledOn(3, year)
+        infix fun April(year: Int) = setScheduledOn(4, year)
+        infix fun May(year: Int) = setScheduledOn(5, year)
+        infix fun June(year: Int) = setScheduledOn(6, year)
+        infix fun July(year: Int) = setScheduledOn(7, year)
+        infix fun August(year: Int) = setScheduledOn(8, year)
+        infix fun September(year: Int) = setScheduledOn(9, year)
+        infix fun October(year: Int) = setScheduledOn(10, year)
+        infix fun November(year: Int) = setScheduledOn(11, year)
+        infix fun December(year: Int) = setScheduledOn(12, year)
     }
-
-    infix fun January(year: Int) = setScheduledOn(1, year)
-    infix fun February(year: Int) = setScheduledOn(2, year)
-    infix fun March(year: Int) = setScheduledOn(3, year)
-    infix fun April(year: Int) = setScheduledOn(4, year)
-    infix fun May(year: Int) = setScheduledOn(5, year)
-    infix fun June(year: Int) = setScheduledOn(6, year)
-    infix fun July(year: Int) = setScheduledOn(7, year)
-    infix fun August(year: Int) = setScheduledOn(8, year)
-    infix fun September(year: Int) = setScheduledOn(9, year)
-    infix fun October(year: Int) = setScheduledOn(10, year)
-    infix fun November(year: Int) = setScheduledOn(11, year)
-    infix fun December(year: Int) = setScheduledOn(12, year)
 }
+
